376c14d2afda507cb80e106c625db1eed1fc3331
Andy Jack <github@veracity.ca>
2019-02-26 14:37:25 +0000

    Choose between setting a value, or working interactively

--- focuscam.sh
+++ focuscam.sh
@@ -2,13 +2,24 @@
 
 deviceNb=0
 
+focus() {
+    echo "Setting focus to $1"
+    v4l2-ctl -d ${deviceNb} -c focus_absolute=$1
+}
+
+# turn off autofocus
 v4l2-ctl -d ${deviceNb} -c focus_auto=0
 
-FOCUS=${1:-10}
+if [ -n $1 ]; then
+    focus $1
+    exit;
+fi
+
+FOCUS=0
 while true; do
-    v4l2-ctl -d ${deviceNb} -c focus_absolute=$FOCUS
+    focus $FOCUS
     echo "Focus level is $FOCUS; ctrl-c if you're happy"
-    echo "Press enter to adjust focus"
+    echo "Press enter to adjust focus by +5"
     read
     FOCUS=$[$FOCUS+5]
 done
